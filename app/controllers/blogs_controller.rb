d920d60aabbf2bc12e67f26f20294bcd65b311a7
AJ <ajarosinski@gmail.com>
2018-08-21 15:40:43 +0300

    Implemented custom scope for blog status and ordered by recent posts

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,7 +8,12 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.page(params[:page]).per(5)
+    if logged_in?(:site_admin)
+      @blogs = Blog.all_paginated(params[:page])
+    else
+      @blogs = Blog.published_paginated(params[:page])
+    end
+
     @page_title = "My Portfolio Blog"
   end
 
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -16,4 +16,12 @@ class Blog < ApplicationRecord
   def self.featured_blogs
   	limit(2)
   end
+
+  def self.all_paginated page
+    page(page).per(5).order(created_at: :desc)
+  end
+
+  def self.published_paginated page
+    published.all_paginated(page)
+  end
 end
