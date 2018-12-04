a14316b1936db87a18870b2510a924fbe3f056a6
AJ <ajarosinski@gmail.com>
2018-12-04 23:15:21 +0300

    Update page title

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
 
   def set_page_defaults
-  	@page_title = "Devcamp Portfolio | My Portfolio Website"
+  	@page_title = "AJ Jarosinski | mustangJaro"
   	@seo_keywords = "AJ Jaro Portfolio"
   end
 end
