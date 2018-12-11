cbe425b37fbf4c7ffc286e89e9b9024584fe6508
AJ <ajarosinski@gmail.com>
2018-12-12 00:15:43 +0300

    Remove register/login links from navigation

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
   end
-  get 'angular-items', to: 'portfolios#angular'
+  
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   get 'about-me', to: 'pages#about'
